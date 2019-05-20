d18da10bfa8afda88addfe982c5dd1d301bcef0d
Jon Ander Oribe <jonan.oribe@gmail.com>
2019-05-20 20:07:21 +0200

    Update main.c

--- C/treesExample/main.c
+++ C/treesExample/main.c
@@ -1,6 +1,11 @@
 #include <stdio.h>
 #include "trees.h"
 
+/*
+Preorder: 5 3 1 4 8 6 9 
+Inorder: 1 3 4 5 6 8 9 
+Postorder: 1 4 3 6 9 8 5 
+*/
 int main() {
         struct tree *t;
         t=tree_init(5,tree_init(3,tree_init(1,NULL,NULL),tree_init(4,NULL,NULL)),tree_init(8,tree_init(6,NULL,NULL),tree_init(9,NULL,NULL)));
