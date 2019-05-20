542b956488f6247081b29e16255decd8c3b25c9b
Jon Ander Oribe <jonan.oribe@gmail.com>
2019-05-20 19:41:40 +0200

    Create trees.c

---
+++ C/treesExample/trees.c
@@ -0,0 +1,8 @@
+#include "trees.h"  /* Include the header (not strictly necessary here) */
+
+int tree_isEmpty(struct tree *t){return 0;}
+struct tree * tree_init(int dato, struct tree *left, struct tree *right){return 0;};
+void tree_destroy(struct tree **t){return;}
+void tree_preorder(struct tree *t){return;}
+void tree_inorder(struct tree *t){return;}
+void tree_postorder(struct tree *t){return;}
