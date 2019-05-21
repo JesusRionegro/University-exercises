b447a0ad0721f65e78051f56beb14c2b8ff8d811
Jon Ander Oribe <jonan.oribe@gmail.com>
2019-05-21 19:20:09 +0200

    Update trees.c

--- C/treesExample/trees.c
+++ C/treesExample/trees.c
@@ -1,12 +1,21 @@
 #include <stdio.h>
 #include "trees.h"  /* Include the header (not strictly necessary here) */
 
-int tree_isEmpty(struct tree *t){return 0;}
-struct tree * tree_init(int dato, struct tree *left, struct tree *right){return 0;};
+int tree_isEmpty(struct tree *t){
+  printf( "¿Esta el arbol vacio? %d\n", (int)t);
+  return 0;}
+struct tree * tree_init(int dato, struct tree *left, struct tree *right){
+  //printf( "El valor en el tree_init es: %d\n", (int)tree);
+  return 0;};
 void tree_destroy(struct tree **t){
   printf( "Destruyendo el arbol : %d\n", (int)*t);
   return;}
-void tree_preorder(struct tree *t){return;}
-void tree_inorder(struct tree *t){return;}
-void tree_postorder(struct tree *t){return;}
-
+void tree_preorder(struct tree *t){
+  printf( "El valor en el tree_preorder es: %d\n", (int)t);
+  return;}
+void tree_inorder(struct tree *t){
+    printf( "El valor en el tree_inorder es: %d\n", (int)t);
+  return;}
+void tree_postorder(struct tree *t){
+    printf( "El valor en el tree_postorder es: %d\n", (int)t);
+  return;}
