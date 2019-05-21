6e6d09b4bb10f96a90918d0b6ed8ca7a30186834
Jon Ander Oribe <jonan.oribe@gmail.com>
2019-05-21 19:32:18 +0200

    Update trees.c

--- C/treesExample/trees.c
+++ C/treesExample/trees.c
@@ -4,18 +4,25 @@
 int tree_isEmpty(struct tree *t){
   printf( "¿Esta el arbol vacio? %d\n", (int)t);
   return 0;}
+
 struct tree * tree_init(int dato, struct tree *left, struct tree *right){
-  //printf( "El valor en el tree_init es: %d\n", (int)tree);
+  printf( "El valor en el tree_init es: %d\n", dato);
   return 0;};
+  
 void tree_destroy(struct tree **t){
   printf( "Destruyendo el arbol : %d\n", (int)*t);
   return;}
+
 void tree_preorder(struct tree *t){
   printf( "El valor en el tree_preorder es: %d\n", (int)t);
   return;}
+
 void tree_inorder(struct tree *t){
     printf( "El valor en el tree_inorder es: %d\n", (int)t);
   return;}
+
 void tree_postorder(struct tree *t){
     printf( "El valor en el tree_postorder es: %d\n", (int)t);
   return;}
+
+
