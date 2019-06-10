c3cd49667a8c01d7105c2b7072a0c60eb5869921
Jon Ander Oribe <jonan.oribe@gmail.com>
2019-06-10 18:14:55 +0200

    Update main.c

--- C/textReader/main.c
+++ C/textReader/main.c
@@ -1,18 +1,24 @@
 #include <stdio.h>
+#include <string.h>
  
 #define MAXCHAR 1000
 int main() {
     FILE *fp;
     char str[MAXCHAR];
-    char* filename = "test.txt";
+    char* filename = "frases.txt";
+
+	int count=0;
+
  
     fp = fopen(filename, "r");
     if (fp == NULL){
-        printf("Could not open file %s",filename);
+        printf("No se pudo abrir el archivo %s",filename);
         return 1;
     }
     while (fgets(str, MAXCHAR, fp) != NULL)
-        printf("%s", str);
+	count = strlen(str);
+        printf("%s %d\n", str, count);
     fclose(fp);
+
     return 0;
 }
